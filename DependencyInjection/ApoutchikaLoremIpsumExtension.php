61d497a8f903189f3865e1c2052d5d3ceb8435cc
apoutchika <contact@julienphilippon.fr>
2013-03-16 22:55:36 +0100

    delete a useless comment

--- DependencyInjection/ApoutchikaLoremIpsumExtension.php
+++ DependencyInjection/ApoutchikaLoremIpsumExtension.php
@@ -18,57 +18,11 @@ class ApoutchikaLoremIpsumExtension extends Extension
       * {@inheritDoc}
       */
      public function load(array $configs, ContainerBuilder $container)
-     {
-	  
-	  /*
-	  $configuration = new Configuration();
-	  $config = $this->processConfiguration($configuration, $configs);
-	  */
-	  
+     {	  
 	  $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
 	  $loader->load('services.yml');
-
-	  /*
-	  echo '<pre>';
-	  print_r ($configs);
-	  die();
-	  //*/
-	  
-	  /*
-	   * The parameters of this bundle is :
-	   *
-	   * apoutchika.lorem_ipsum:
-	   *   paragraphs:
-	   *     min: 2
-	   *     max: 8
-	   *   sentences:
-	   *     min: 2
-	   *     max: 8
-	   *   words:
-	   *     min: 2
-	   *     max: 8
-	   *   quantity_punctuation: 10
-	   *   lorem_ipsum : >
-	   *     lorem ipsum dolor sit amet consectetur adipiscing elit sed non 
-	   *     risus suspendisse lectus tortor dignissim nec ultricies cras elementum ultrices
-	   *     diam maecenas ligula massa varius a semper congue euismod mi
-	   *     proin porttitor orci nonummy molestie enim est eleifend fermentum
-	   *     nisl erat duis arcu scelerisque vitae consequat in pretium pellentesque 
-	   *     ut volutpat libero pharetra tempor vestibulum bibendum augue praesent 
-	   *     egestas leo pede blandit odio eu dui sodales ante primis faucibus 
-	   *     luctus et posuere cubilia curae aliquam nibh mauris ac hendrerit velit 
-	   *     gravida ornare aenean vel suscipit pulvinar nulla sollicitudin fusce tempus 
-	   *     nunc turpis ullamcorper sapien eros rhoncus integer id felis curabitur 
-	   *     aliquet quis metus lobortis consectetuer morbi convallis vehicula tellus 
-	   *     quam feugiat purus iaculis tristique justo magna at mollis vulputate 
-	   *     sem vivamus placerat imperdiet cursus rutrum tincidunt lacus
-	   */
-
 	
 	  $typeMinAndMax = array ('paragraphs', 'sentences', 'words');
-
-
-	  
 	  
 	  foreach ($configs[0] as $type => $value)
 	  {
